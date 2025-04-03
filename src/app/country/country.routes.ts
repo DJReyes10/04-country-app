04bdce19c0ea20ffa47e70e3a9aca8a17e13bbd4
DJReyes10 <djreyesp@unah.hn>
2025-04-03 10:13:32 -0600

    Creación de proyecto de Angular, sección 9

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from "@angular/router";
+import { ByCapitalPageComponent } from "./Pages/by-capital-page/by-capital-page.component";
+
+export const countryRoutes: Routes = [
+    {
+        path: '',
+        component: ByCapitalPageComponent
+    },
+    // {
+    //     path: '**',
+    // redirectTo:    '',
+    // }
+];
+
+export default countryRoutes;
