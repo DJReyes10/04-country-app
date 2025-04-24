07336727c8b7d7c6a9bd8e23cd31fa4fde2a4486
DJReyes10 <djreyesp@unah.hn>
2025-04-24 14:34:36 -0600

    Refactor y mejora de la funcionalidad de búsqueda de países por capital, incluyendo la implementación de señales para manejar el estado de carga y errores, y la actualización de componentes para mostrar la lista de países correctamente.

---
+++ src/app/country/mappers/country.mapper.ts
@@ -0,0 +1,33 @@
+import type { Country } from '../interfaces/country.interface';
+import type { RESTCountry } from '../interfaces/rest-countries.interface';
+
+export class CountryMapper {
+  static mapRestCountryToCountry(restCountry: RESTCountry): Country {
+    return {
+      capital: restCountry.capital.join(','),
+      cca2: restCountry.cca2,
+      flag: restCountry.flags.png,
+      flagSvg: restCountry.flags.svg,
+      // Necesito mostrar el nombre en español, por eso uso el español como idioma por defecto
+      name: restCountry.translations['spa']?.common ?? 'No Spanish name found',
+      //   capital: restCountry.capital?.[0] ?? '',
+      population: restCountry.population,
+    } as Country;
+  }
+  //   static toDomain(country: RESTCountry): Country {
+  //     return {
+  //       cca2: country.cca2,
+  //       flag: country.flags.png,
+  //       flagSvg: country.flags.svg,
+  //       name: country.name.common,
+  //       capital: country.capital?.[0] ?? '',
+  //       population: country.population,
+  //     };
+  //   }
+  static mapRestCountryArrayToCountryArray(
+    resCountries: RESTCountry[]
+  ): Country[] {
+    return resCountries.map(this.mapRestCountryToCountry);
+    // return resCountries.map((country) => this.mapRestCountryToCountry(country));
+  }
+}
