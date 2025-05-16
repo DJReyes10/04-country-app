0b5fb1ccc802a2c8e25e8b66e01abfbfb1c41aad
DJReyes10 <djreyesp@unah.hn>
2025-05-16 16:47:46 -0600

    Refactor la búsqueda de países por capital para utilizar un recurso reactivo y mejorar la gestión de errores en el servicio.

--- src/app/country/Pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/Pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { first, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,28 +12,43 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
 
-    this.isLoading.set(true);
-    this.isError.set(null);
+  //Este componente es un ejemplo de como se puede usar el servicío de froma reactiva
+  //Pero no es necesario utilizarlo de esta forma, ya que el servicio ya tiene un método para buscar por capital
+  //
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
 
-    this.countryService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(
-          'No se encontraron resultados para la búsqueda: ' + query
-        );
-      },
-    });
-  }
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(
+  //         'No se encontraron resultados para la búsqueda: ' + query
+  //       );
+  //     },
+  //   });
+  // }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable } from 'rxjs';
+import { map, Observable, catchError, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -15,11 +15,16 @@ export class CountryService {
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp))
-      );
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching', error);
+
+        return throwError(
+          () => new Error('No se puedo obtener países con ese query')
+        );
+      })
+    );
   }
   searchByCountry(query2: string) {
     query2 = query2.toLowerCase();
