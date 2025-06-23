2dd30c24bfecb7027111dafc2adf8307e57364c3
DJReyes10 <djreyesp@unah.hn>
2025-06-23 15:17:58 -0600

    Refactor y mejora la búsqueda de países por capital y país, implementando caché para resultados y optimizando la gestión de entradas con debounce.

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -6,7 +6,20 @@ import { Component, input, output } from '@angular/core';
   templateUrl: './country-search-input.component.html',
 })
 export class CountrySearchInputComponent {
-
   placerholder = input('country-search-input.placeholder');
   value = output<string>();
- }
+  debounceTime = input(400);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  }, {});
+}
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -15,11 +15,21 @@ export class CountryService {
     throw new Error('Method not implemented.');
   }
   private http = inject(HttpClient);
-
+  private queryCacheCapital = new Map<string, Country[]>(); // Mapa para almacenar las consultas anteriores
+  private queryCacheCountry = new Map<string, Country[]>();
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
+
+    // Verifica si el resultado ya está en la caché
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((countries) => {
+        this.queryCacheCapital.set(query, countries); // Almacena el resultado en la cach
+      }),
       catchError((error) => {
         console.log('Error fetching', error);
 
@@ -33,8 +43,18 @@ export class CountryService {
   searchByCountry(query: string): Observable<Country[]> {
     const url = `${API_URL}/name/${query}`;
     query = query.toLowerCase();
+
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+
+    console.log(`Llegando al servidor por ${query}`);
     return this.http.get<RESTCountry[]>(url).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((countries) => {
+        this.queryCacheCountry.set(query, countries); // Almacena el resultado en la caché
+      }),
+      // Simula un retraso de 2 segundos para ver el efecto del debounce
       delay(2000), // Simula un retraso de 1 segundo
       catchError((error) => {
         console.log('Error fetching', error);
